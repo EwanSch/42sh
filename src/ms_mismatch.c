c7f7b52fb81c0a5bc1b49d4f0597755a4025f194
Benjamin <benjamin.hess@epitech.eu>
2026-04-20 14:47:09 +0200

    [Fix] forgot mismatch file

---
+++ src/ms_mismatch.c
@@ -0,0 +1,83 @@
+/*
+** EPITECH PROJECT, 2026
+** minishell1
+** File description:
+** Project's entry point
+** Auto-Generated with Episetup by Amélie
+** Author:
+** Amélie Ambleton--Guth
+** Feb 2026
+*/
+
+#include <stdio.h>
+
+#include "minishell1.h"
+
+static void find_match(char *str, int *stay, int i, char search)
+{
+    if (*stay || str[i] == search) {
+        *stay = 1;
+        if (str[i + 1] == search)
+            return;
+    }
+}
+
+int check_for_match(char *str, char search)
+{
+    int stay = 0;
+
+    for (int i = 0; str[i]; i++) {
+        find_match(str, &stay, i, search);
+    }
+    return stay ? 1 : 0;
+}
+
+static void find_brace(char *str, int *stay, int *buf, int i)
+{
+    if (*stay || str[i] == '{') {
+        if (*stay == 0 && str[i + 1] == ' ') {
+            *buf = 1;
+        }
+        *stay = 1;
+        if (str[i + 1] == '}') {
+            *stay = 0;
+            return;
+        }
+    }
+}
+
+static int check_brace(char *str)
+{
+    int stay = 0;
+    int buf = 0;
+
+    for (int i = 0; str[i]; i++) {
+        find_brace(str, &stay, &buf, i);
+    }
+    if (buf)
+        return 0;
+    return stay == 1 ? 1 : 0;
+}
+
+int all_quote(char *str, int i)
+{
+    if (str[i] == '\"' || str[i] == '\'' || str[i] == '`') {
+        if (check_for_match(str, str[i])) {
+            printf("Unmatched '%c'.\n", str[i]);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int mismatch(char *str)
+{
+    for (int i = 0; str[i]; i++)
+        if (all_quote(str, i))
+            return 1;
+    if (check_brace(str)) {
+        printf("Missing '}'.\n");
+        return 1;
+    }
+    return 0;
+}
