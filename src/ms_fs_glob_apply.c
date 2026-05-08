1a6059850be67e0a3fc5333da1c0c6967b26d841
AisakaPMD <amelie.ambletonguth@gmail.com>
2026-05-08 19:20:06 +0200

    [Fix] Wrong condition

--- src/ms_fs_glob_apply.c
+++ src/ms_fs_glob_apply.c
@@ -93,7 +93,7 @@ int apply_globbing(list_t **tokens,
 
     if (!cur)
         return 0;
-    if (my_strcmp("if", ((ms_token_t *) (*tokens)->data)->word_value))
+    if (my_strcmp("if", ((ms_token_t *) (*tokens)->data)->word_value) == 0)
         return 0;
     init_gctx(&gctx, tokens, ctx);
     while (cur) {
